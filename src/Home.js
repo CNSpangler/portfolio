65697a1c5c50b1cbf3915822b8dc9b6eb8fc49e7
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-25 21:45:20 -0700

    side-by-side formatting set up

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,23 @@ import ReactDOM from 'react-dom';
 export default class Home extends Component {
   render() {
     return (
-      <div class="main">
-        <section class="nameplate gleam">
-          <span class="name">Chelsea Spangler</span>
-        </section>
-        <br /><br />
-        <div class="title rivet"></div>
-      </div>
+      <>
+        <div class="main">
+          <section class="nameplate gleam">
+            <span class="name">Chelsea Spangler</span>
+          </section>
+          <div class="title rivet"></div>
+        </div>
+
+        <div class="side-by-side">
+          <div id="left" class="info">
+            Hi, I'm Chelsea. I build useful apps that solve real-world problems.
+          </div>
+          <div id="right" class="info">
+            Here is some more stuff
+          </div>
+        </div>        
+      </>
     )
   }
 }
