0139e8061902a09ee316f36a278aac50c2542269
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-31 16:48:27 -0700

    add contact icons and resume link

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,6 @@ export default class Home extends Component {
         <div class="main">
           <section class="nameplate gleam">
             <span class="name">Chelsea Spangler</span>
-            <span class="name-shadow">Chelsea Spangler</span>
-            <span class="name-placeholder">Chelsea Spangler</span>
           </section>
           <div class="title rivet"></div>
         </div>
@@ -18,9 +16,15 @@ export default class Home extends Component {
           <div id="left" class="info">
             Hi, I'm Chelsea. I build useful apps that solve real-world problems.
           </div>
-          <div id="right" class="info">
-            Here is some more stuff
-          </div>
+          <ul id="right" class="info">
+            <li>
+              <a href="https://www.canva.com/design/DAD5iEB56sc/5d2MYtNBsnSOXzPhpKT6Qg/view?utm_content=DAD5iEB56sc&utm_campaign=designshare&utm_mediumank&utm_source=sharebutton">
+                <img class="icon" src="resume.png" alt="Resume icon" />
+                <span>Resume</span>
+              </a>
+            </li>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+          </ul>
         </div>        
       </>
     )
